c1b43ed3eaf164ea4b6323e6a4f2d75f2910b760
Sandesh N <sandesh.n.dev@gmail.com>
2026-08-01 08:28:14 +0530

    Initial commit with L39 of CodeHelp's web dev playlist

---
+++ CodeHelp/L39/index.js
@@ -0,0 +1,10 @@
+
+/** To print this in terminal, navigate to this folder or enter this path+filename
+    node index.js -> if inside this folder
+    node codehelp/L39/index.js  -> From JsCodeBase
+
+    A quick pro-tip for your terminal workflow:
+    You can actually drop the .js extension when running the file.
+    Node.js is smart enough to look for JavaScript files by default.
+*/
+console.log("Hello world again, but from file");
